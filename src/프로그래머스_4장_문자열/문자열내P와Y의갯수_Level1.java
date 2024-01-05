8cece7e131ba9decc812b1959e05849d8641cc6e
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-06 02:01:17 +0900

    feat(programmers): level1

--- src/프로그래머스_4장_문자열/문자열내P와Y의갯수_Level1.java
+++ src/프로그래머스_4장_문자열/문자열내P와Y의갯수_Level1.java
@@ -6,16 +6,11 @@ package 프로그래머스_4장_문자열;
  2. y의 갯수를 센다
  3. 대소문자 구분은 없으며, 같으면 true 다르면 false를 리턴한다. 둘다 0이면 true를 리턴한다.
  **/
-public class 문자열내P와Y의갯수 {
+public class 문자열내P와Y의갯수_Level1 {
     boolean solution(String s) {
         s = s.toUpperCase();
 
-        int p = Pcount(s);
-        int y = Ycount(s);
-
-        if(p == y && p == 0) return true;
-
-        return p == y;
+        return Pcount(s) == Ycount(s);
     }
 
     private int Pcount (String s){
@@ -27,5 +22,12 @@ public class 문자열내P와Y의갯수 {
         return (int) s.chars()
                 .filter(o -> o == 'Y')
                 .count();
+    }
+
+    boolean solution2(String s) {
+        s = s.toUpperCase();
+
+        return s.chars().filter(o -> o == 'P').count() ==
+                s.chars().filter(o -> o == 'Y').count();
     }
 }
