b3810039e1e337075ecd0f5f1c21cbd2d6b5458a
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-05 16:37:14 +0900

    feat(programmers): level1

--- src/프로그래머스_4장_문자열/자연수뒤집어배열로만들기_Level1.java
+++ src/프로그래머스_4장_문자열/자연수뒤집어배열로만들기_Level1.java
@@ -1,14 +1,26 @@
 package 프로그래머스_4장_문자열;
 
 import java.util.*;
+
 public class 자연수뒤집어배열로만들기_Level1 {
     public int[] solution(long n) {
-        String str = String.valueOf(n);
-        str = new StringBuilder(str).reverse().toString();
+        String str = new StringBuilder(String.valueOf(n)).reverse().toString();
         String[] repo = str.split("");
 
         return Arrays.stream(repo)
                 .mapToInt(Integer::parseInt)
                 .toArray();
+    }
+
+    public int[] bookSolution(long n) {
+        String str = Long.toString(n);
+        String reversed = new StringBuilder(str).reverse().toString();
+        char[] arr = reversed.toCharArray();
+
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i] - '0';
+        }
+        return result;
     }
 }
