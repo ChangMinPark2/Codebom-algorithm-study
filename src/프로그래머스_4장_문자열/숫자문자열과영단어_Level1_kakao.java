833f8525650f942592e98f26932abdc8546aa0f3
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-06 02:08:19 +0900

    feat(programmers): level1_kakao

---
+++ src/프로그래머스_4장_문자열/숫자문자열과영단어_Level1_kakao.java
@@ -0,0 +1,18 @@
+package 프로그래머스_4장_문자열;
+
+public class 숫자문자열과영단어_Level1_kakao {
+    public int solution(String s) {
+        s = s.replace("zero", "0");
+        s = s.replace("one", "1");
+        s = s.replace("two", "2");
+        s = s.replace("three", "3");
+        s = s.replace("four", "4");
+        s = s.replace("five", "5");
+        s = s.replace("six", "6");
+        s = s.replace("seven", "7");
+        s = s.replace("eight", "8");
+        s = s.replace("nine", "9");
+
+        return Integer.parseInt(s);
+    }
+}
