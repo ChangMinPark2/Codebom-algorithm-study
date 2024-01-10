95a69a83df74fc430549596a8f87d1e098a59d92
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-10 14:34:35 +0900

    feat(코드봄): 해시_level2

---
+++ src/코드봄/해시/전화번호목록.java
@@ -0,0 +1,22 @@
+package 코드봄.해시;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ 1. startsWith
+ **/
+public class 전화번호목록 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
