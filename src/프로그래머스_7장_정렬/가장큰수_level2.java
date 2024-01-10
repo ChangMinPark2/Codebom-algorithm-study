5faed0b50a8770f980e627c9bfffabb8d48f77ff
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-11 00:07:03 +0900

    feat(코드봄): 정렬_level2

--- src/프로그래머스_7장_정렬/가장큰수_level2.java
+++ src/프로그래머스_7장_정렬/가장큰수_level2.java
@@ -16,4 +16,19 @@ public class 가장큰수_level2 {
                 .collect(Collectors.joining(""))
                 .replaceAll("^0+", "0"); // {0, 0, 0} 일 경우 -> 0으로 치환해해준다
     }
+
+    public String solution2(int[] numbers) {
+        String answer = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((o1,o2) -> {
+                    int origin = Integer.parseInt(o1 + o2);
+                    int after = Integer.parseInt(o2 + o1);
+                    return after - origin;
+                })
+                .collect(Collectors.joining(""));
+
+        answer = answer.replaceAll("^0+", "0");
+
+        return answer;
+    }
 }
