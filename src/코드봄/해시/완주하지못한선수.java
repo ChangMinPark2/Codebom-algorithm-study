d8ba0a79c3ea2458b105d49499ccee6050b6fa42
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-10 12:24:01 +0900

    feat(코드봄): 해시_level1

--- src/코드봄/해시/완주하지못한선수.java
+++ src/코드봄/해시/완주하지못한선수.java
@@ -2,6 +2,12 @@ package 코드봄.해시;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
+/**
+ * 1. 참여자들을 +1 해준다.
+ * 2. 완주자들을 -1 해준다.
+ * 3. value 값이 0이 아닌 것을 리턴한다.
+ */
 public class 완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
