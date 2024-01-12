41f50d1d07bc03cc78856c8186a6fd203845e034
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-12 15:33:34 +0900

    feat(programmers): 동적프로그래밍_Level2

---
+++ src/프로그래머스_10장_동적프로그래밍/피보나치수_Level2.java
@@ -0,0 +1,32 @@
+package 프로그래머스_10장_동적프로그래밍;
+
+import java.util.Arrays;
+
+/**
+ <문제풀이흐름>
+ n의 최대: 10만, 재귀의 깊이가 1만이 넘어가면 StackOverflowError가 뜨므로 일반 재귀는 불가.
+ 동적 프로그래밍 이용 -> 애도 첫 시작이 십만이기 때문에, StackOverFlow가 남.
+ < 작은 부분 문제부터 해결하기 >
+ **/
+public class 피보나치수_Level2 {
+    private int[] mem = new int[100001];
+
+    public int solution(int n) {
+
+        Arrays.fill(mem, -1);
+
+        for(int i = 0; i <= n; i++) {
+            fib(n);
+        }
+        return fib(n);
+    }
+
+    private int fib(int n) {
+        if(mem[n] != -1) return mem[n];
+        if(n == 1 || n == 0) {
+            return n;
+        }
+
+        return mem[n] = (fib(n-1) + fib(n-2)) % 1234567;
+    }
+}
