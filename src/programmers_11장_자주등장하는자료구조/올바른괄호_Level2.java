f23cef284999542520a8bc3b6864e391f583a715
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-13 01:13:51 +0900

    feat(programmers): 스택_Level2

---
+++ src/programmers_11장_자주등장하는자료구조/올바른괄호_Level2.java
@@ -0,0 +1,26 @@
+package programmers_11장_자주등장하는자료구조;
+/**
+ * <문제풀이흐름>
+ * 스택 선언
+ * (이면 푸쉬  )이면 팝
+ * 조건 추가 -> 스택이 비어있을 때 ) 이 들어오면 false 리턴
+ **/
+
+import java.util.*;
+
+public class 올바른괄호_Level2 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(c);
+
+            if (c == ')') {
+                if (stack.isEmpty()) return false;
+
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
