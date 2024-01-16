45a18e67c5cda34cfbef21602ee022be67bc032a
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-16 23:05:57 +0900

    feat(programmers): Level2

---
+++ src/코드봄/스택큐/올바른괄호.java
@@ -0,0 +1,21 @@
+package 코드봄.스택큐;
+
+import java.util.*;
+
+public class 올바른괄호 {
+    boolean solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()) {
+            if (c == '('){
+                stack.push(c);
+            }
+            if (c == ')'){
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
