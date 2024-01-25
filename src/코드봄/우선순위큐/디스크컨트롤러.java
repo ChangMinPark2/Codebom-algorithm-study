947cfe3525c815ed4b2c613d5d05c3ccd64f76de
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-25 12:43:59 +0900

    feat(programmers): 디스크컨트롤러

---
+++ src/코드봄/우선순위큐/디스크컨트롤러.java
@@ -0,0 +1,45 @@
+package 코드봄.우선순위큐;
+
+import java.util.*;
+
+/**
+ * <문제풀이흐름>
+ * [요청 시점, 작업의 소요 시간] 작업의 요청부터 종료까지 걸린 시간의 평균이 가장 낮은 값.
+ * 하드 디스크가 작업을 수행하지 않을 때는 먼저 요청이 들어온 작업부터 처리한다.
+ * 요청이 들어온 작업 중에서 수행 시간이 짧은 것을 선택하는 문제이다.
+ * <p>
+ * 배열의 요청시간이 짧은 순으로 온다는 말이 없다. -> 0번째 인덱스 기준 오름차순 정렬
+ * 작업이 끝나는 시점에 요청이 들어온 작업 중 작업의 소요 시간이 낮은 선택.
+ * -> (요청이 들어온 작업 중) pq 1번째 인덱스 기준 최솟값을 가지는 힙 선언
+ */
+public class 디스크컨트롤러 {
+
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        int sum = 0;
+        int count = 0;
+        int index = 0;
+        int endTime = 0;
+
+        while (count < jobs.length) {
+
+            while (index < jobs.length && jobs[index][0] <= endTime) {
+                pq.add(jobs[index]);
+                index++;
+            }
+
+            if (!pq.isEmpty()) {
+                int[] arr = pq.poll();
+                sum += endTime - arr[0] + arr[1];
+                endTime += arr[1];
+                count++;
+            } else {
+                endTime = jobs[index][0];
+            }
+        }
+        System.out.println(sum);
+        return sum / jobs.length;
+    }
+}
