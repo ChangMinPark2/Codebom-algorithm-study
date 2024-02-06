6743cf900bfb8c8e06593dde5d32d706602b3c1d
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-07 00:15:54 +0900

    feat(백준): 숨바꼭질 - bfs

---
+++ src/백준/숨바꼭질.java
@@ -0,0 +1,49 @@
+package 백준;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * BFS -> result -1, result +1; result * 2 를 큐에 넣는다.
+ * depth가 target일때 return;
+ */
+public class 숨바꼭질 {
+    static int now, target;
+    static boolean[] visited = new boolean[100001]; // 방문 여부를 체크할 배열. 위치가 10만까지 가능하므로.
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        now = Integer.parseInt(input[0]);
+        target = Integer.parseInt(input[1]);
+
+        System.out.println(bfs(now));
+    }
+
+    static int bfs(int now) {
+        q.add(new int[]{now, 0});
+        visited[now] = true;
+
+        while (!q.isEmpty()) {
+            int[] standard = q.poll();
+
+            if (standard[0] == target) {
+                return standard[1];
+            }
+
+            isCheck(standard[0] - 1, standard[1]);
+            isCheck(standard[0] + 1, standard[1]);
+            isCheck(standard[0] * 2, standard[1]);
+        }
+        return 0;
+    }
+    private static void isCheck(int x, int y) {
+        if (x >= 0 && x <= 100000 && !visited[x]) {
+            q.add(new int[] {x, y + 1});
+            visited[x] = true;
+        }
+    }
+}
+
+
