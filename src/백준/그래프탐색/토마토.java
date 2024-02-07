795278b8a212ad625986705e0876ecc48730710e
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-07 15:47:28 +0900

    feat(백준): 토마토 - bfs_3차원 배열

--- src/백준/그래프탐색/토마토.java
+++ src/백준/그래프탐색/토마토.java
@@ -13,12 +13,12 @@ import java.io.*;
  **/
 public class 토마토 {
     static int[][][] graph;
-    static boolean[][][] visited;
     static int m, n, h, answer;
     static int[] dx = {0, 0, -1, 1, 0, 0}; //위 아래 왼쪽 오른쪽 앞 뒤
     static int[] dy = {0, 0, 0, 0, -1, 1};
     static int[] dh = {-1, 1, 0, 0, 0, 0};
     static Queue<int[]> q = new LinkedList<>();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
@@ -27,8 +27,6 @@ public class 토마토 {
         h = Integer.parseInt(input[2]); //높이
 
         graph = new int[h][n][m];
-        visited = new boolean[h][n][m];
-
         //그래프 채우기
         for (int z = 0; z < h; z++) {
             for (int i = 0; i < n; i++) {
@@ -39,13 +37,11 @@ public class 토마토 {
             }
         }
 
-        answer = 0;
-
         //탐색
         for (int z = 0; z < h; z++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
-                    if (!visited[z][i][j] && graph[z][i][j] == 1) {
+                    if (graph[z][i][j] == 1) {
                         q.add(new int[]{z, i, j, 0});
                     }
                 }
@@ -73,15 +69,12 @@ public class 토마토 {
     }
 
     static void bfs(int z, int y, int x, int count) {
-        visited[z][y][x] = true;
-
         for (int i = 0; i < dx.length; i++) {
             int nh = dh[i] + z;
             int ny = dy[i] + y;
             int nx = dx[i] + x;
 
-            if (isCheck(nh, ny, nx) && !visited[nh][ny][nx] && graph[nh][ny][nx] == 0) {
-                visited[nh][ny][nx] = true;
+            if (isCheck(nh, ny, nx) && graph[nh][ny][nx] == 0) {
                 graph[nh][ny][nx] = 1;
                 q.add(new int[]{nh, ny, nx, count + 1});
             }
