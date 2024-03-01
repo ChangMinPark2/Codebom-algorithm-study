40a923f2ef570ce354d3739c066fb71c584cb56d
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-01 23:54:53 +0900

    feat(백준) : re

---
+++ src/백준_Re/S3_N과M.java
@@ -0,0 +1,14 @@
+package 백준_Re;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class S3_N과M {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]); //4
+        int b = Integer.parseInt(input[1]); //2
+
+    }
+}
