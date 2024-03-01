a1be5117bdd04f0a31a9139b1e61027e53fbe92d
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-02 00:00:46 +0900

    feat(PCCP) : 1번

--- src/PCCP/Level1_붕대감기.java
+++ src/PCCP/Level1_붕대감기.java
@@ -1,7 +1,10 @@
 package PCCP;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Level1_붕대감기 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
