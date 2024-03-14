05a3927e5fdc84d0c55b58585cb9180e1f3c5ddc
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-14 10:10:03 +0900

    feat(프로그래머스) : Level3

--- src/코드봄/디피/Level3_등굣길.java
+++ src/코드봄/디피/Level3_등굣길.java
@@ -3,8 +3,8 @@ package 코드봄.디피;
 import java.util.*;
 
 public class Level3_등굣길 {
-    static int[][] mem = new int[101][101];
-    public int solution(int m, int n, int[][] puddles) {
+    static int[][] mem = new int[5][4];
+    public static int solution(int m, int n, int[][] puddles) {
         for(int[] temp : mem) {
             Arrays.fill(temp, -1);
         }
@@ -18,7 +18,7 @@ public class Level3_등굣길 {
         return count(1, 1, m, n, isPuddle);
     }
 
-    private int count(int x, int y, int w, int h, boolean[][] isPuddle) {
+    private static int count(int x, int y, int w, int h, boolean[][] isPuddle) {
         if(x > w || y > h) return 0;
         if(isPuddle[y][x]) return 0;
 
@@ -26,6 +26,12 @@ public class Level3_등굣길 {
         if(x == w && y == h) return 1;
 
         int total = count(x + 1, y, w, h, isPuddle) + count(x, y + 1, w, h, isPuddle);
+
         return mem[x][y] = total % 1000000007;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(mem));
+        System.out.println(solution(4, 3, new int[][] {{2, 2}}));
     }
 }
