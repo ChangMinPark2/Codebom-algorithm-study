4221cca73b8da7dc7527198dc8d36e13d7e2e4b7
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-03 01:26:37 +0900

    feat(백준): 카카오 Level2_튜플

---
+++ src/코드봄_level2/카카오/카카오level2_튜플.java
@@ -0,0 +1,28 @@
+package 코드봄_level2.카카오;
+
+import java.util.*;
+
+public class 카카오level2_튜플 {
+	public int[] solution(String s) {
+		s = s.replace("{{", "");
+		s = s.replace("}}", "");
+		s = s.replace("},{", " ");
+		String[] str = s.split(" ");
+		Arrays.sort(str, (o1, o2) -> o1.length() - o2.length());
+		List<Integer> list = new ArrayList<>();
+
+		for(String a: str) {
+			if(a.length() == 1) {
+				list.add(Integer.parseInt(a));
+			} else {
+				String[] temp = a.split(",");
+				for(String z : temp) {
+					if (!list.contains(Integer.parseInt(z))) {
+						list.add(Integer.parseInt(z));
+					}
+				}
+			}
+		}
+		return list.stream().mapToInt(Integer::intValue).toArray();
+	}
+}
