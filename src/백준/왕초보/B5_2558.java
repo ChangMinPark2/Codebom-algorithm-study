ba30f2f0440eec7f6acd796b0ae6957c8c78d5e5
CHANG MIN PARK <98qkrckdals@naver.com>
2024-07-20 14:31:20 +0900

    feat(백준): B5_2558

--- src/백준/왕초보/B5_2558.java
+++ src/백준/왕초보/B5_2558.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B5_에이플러스비 {
+public class B5_2558 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
