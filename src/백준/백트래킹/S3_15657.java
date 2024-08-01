632d8d4f77131f40596cad08ebd1bd7a952fc79d
CHANG MIN PARK <98qkrckdals@naver.com>
2024-08-01 16:14:43 +0900

    feat(백준): S3_15657

---
+++ src/백준/백트래킹/S3_15657.java
@@ -0,0 +1,45 @@
+package 백준.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S3_15657 {
+    static int[] map;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] repo = br.readLine().split(" ");
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        int n = Integer.parseInt(repo[0]);
+        int m = Integer.parseInt(repo[1]);
+
+        map = new int[m];
+        visited = new boolean[n];
+
+        dfs(n, m, 0, arr, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int n, int m, int depth, int[] arr, int start) {
+        if (depth == m) {
+            for (int i : map) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        //중복은 허용하돼, 낮으면 안 됨.
+        for (int i = start; i < n; i++) {
+            map[depth] = arr[i];
+            dfs(n, m, depth + 1, arr, i);
+        }
+    }
+}
