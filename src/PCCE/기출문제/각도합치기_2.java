a44b06532a4e0a1e596d9c6c00df3e5d150766a6
CHANG MIN PARK <98qkrckdals@naver.com>
2024-09-15 04:07:31 +0900

    feat(프로그래머스): pcce_2번

---
+++ src/PCCE/기출문제/각도합치기_2.java
@@ -0,0 +1,15 @@
+package PCCE.기출문제;
+
+import java.util.Scanner;
+
+public class 각도합치기_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int angle1 = sc.nextInt();
+        int angle2 = sc.nextInt();
+
+        int sum_angle = angle1 + angle2;
+        System.out.println(sum_angle % 360);
+    }
+}
+
