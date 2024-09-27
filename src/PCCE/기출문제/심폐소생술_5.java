bf6be32dd3a98e243a210bef8c3c26c2893d68c6
CHANG MIN PARK <98qkrckdals@naver.com>
2024-09-27 17:44:41 +0900

    feat(프로그래머스): pcce_5번

---
+++ src/PCCE/기출문제/심폐소생술_5.java
@@ -0,0 +1,19 @@
+package PCCE.기출문제;
+
+public class 심폐소생술_5 {
+    public int[] solution(String[] cpr) {
+        int[] answer = {0, 0, 0, 0, 0};
+        String[] basic_order = {"check", "call", "pressure", "respiration", "repeat"};
+
+        for (int i = 0; i < cpr.length; i++) {
+            for (int j = 0; j < basic_order.length; j++) {
+                if (cpr[i].equals(basic_order[j])) {
+                    answer[i] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
