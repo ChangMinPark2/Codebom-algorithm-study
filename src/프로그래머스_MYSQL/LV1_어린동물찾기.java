27c133cb455c6889f0b61670b16be8ff00bdc914
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-26 19:01:37 +0900

    feat(프로그래머스): LEVEL_1_어린동물찾기

---
+++ src/프로그래머스_MYSQL/LV1_어린동물찾기.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_어린동물찾기 {
+    /**
+     * 이 테이블에서 어린 동물에 id와 이름을 조회하고, id순으로 보여주세요
+     * SELECT ANIMAL_ID, NAME
+     * FROM ANIMAL_INS
+     * WHERE INTAKE_CONDITION != 'AGED'
+     * ORDER BY ANIMAL_ID ASC
+     */
+}
