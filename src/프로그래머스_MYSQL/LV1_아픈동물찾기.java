0636cb8b90698724dc5fe6df47f221f30e2a40b8
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-26 18:56:40 +0900

    feat(프로그래머스): LEVEL_1_아픈동물찾기

---
+++ src/프로그래머스_MYSQL/LV1_아픈동물찾기.java
@@ -0,0 +1,12 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_아픈동물찾기 {
+
+    // 테이블에서 아픈 동물의 id와 이름을 id순으로 조회하기
+    /**
+     * SELECT ANIMAL_ID, NAME
+     * FROM ANIMAL_INS
+     * WHERE INTAKE_CONDITION like 'Sick'
+     * ORDER BY ANIMAL_ID ASC
+     */
+}
