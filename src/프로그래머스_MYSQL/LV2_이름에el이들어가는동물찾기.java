5eddc75418410101496bc9df0409d40c383e8beb
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-30 02:26:54 +0900

    feat(프로그래머스): LEVEL_2_이름에el이들어가는동물찾기

---
+++ src/프로그래머스_MYSQL/LV2_이름에el이들어가는동물찾기.java
@@ -0,0 +1,12 @@
+package 프로그래머스_MYSQL;
+
+public class LV2_이름에el이들어가는동물찾기 {
+    /**
+     * 동물 이름 중, 이름에 'EL'이 들어가는 개의 아이디와 이름을 이름 순으로 조회하기.
+     * -- 코드를 입력하세요
+     * SELECT ANIMAL_ID, NAME
+     * FROM ANIMAL_INS
+     * WHERE NAME LIKE '%el%' && ANIMAL_TYPE = 'Dog'
+     * ORDER BY NAME
+     */
+}
