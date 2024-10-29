4e2549fa03cdc4d6ab22600f5045a1dae8b91a3c
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-30 02:29:05 +0900

    feat(프로그래머스): LEVEL1_동물의아이디와이름

---
+++ src/프로그래머스_MYSQL/LV1_동물의아이디와이름.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_동물의아이디와이름 {
+    /**
+     * 동물 보홋소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요.
+     * -- 코드를 입력하세요
+     * SELECT ANIMAL_ID, NAME
+     * FROM ANIMAL_INS
+     * ORDER BY ANIMAL_ID
+     */
+}
