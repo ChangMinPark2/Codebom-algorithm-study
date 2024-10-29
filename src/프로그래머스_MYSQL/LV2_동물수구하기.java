d3534eaf8c914b123690fd9e60c0276051461933
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-30 02:20:49 +0900

    feat(프로그래머스): LEVEL_2_동물수구하기

---
+++ src/프로그래머스_MYSQL/LV2_동물수구하기.java
@@ -0,0 +1,10 @@
+package 프로그래머스_MYSQL;
+
+public class LV2_동물수구하기 {
+    /**
+     * 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성하기
+     * -- 코드를 입력하세요
+     * SELECT count(*)
+     * FROM ANIMAL_INS
+     */
+}
