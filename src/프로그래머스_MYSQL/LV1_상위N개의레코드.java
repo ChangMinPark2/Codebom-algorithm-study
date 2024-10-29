a19b91ddeaade1b009e064c4accf1e4bd6d954f7
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-30 02:49:14 +0900

    feat(프로그래머스): LEVEL1_상위N개의레코드

---
+++ src/프로그래머스_MYSQL/LV1_상위N개의레코드.java
@@ -0,0 +1,12 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_상위N개의레코드 {
+    /**
+     * 동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL문을 작성해주세요.
+     * -- 코드를 입력하세요
+     * SELECT NAME
+     * FROM ANIMAL_INS
+     * ORDER BY DATETIME
+     * LIMIT 1
+     */
+}
