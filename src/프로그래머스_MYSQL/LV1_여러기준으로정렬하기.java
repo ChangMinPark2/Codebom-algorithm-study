82cde0b4b71f296ca0c2dfa38cab90d9d8f227da
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-30 02:42:17 +0900

    feat(프로그래머스): LEVEL1_여러기준으로정렬하기

---
+++ src/프로그래머스_MYSQL/LV1_여러기준으로정렬하기.java
@@ -0,0 +1,10 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_여러기준으로정렬하기 {
+    /**
+     * 모든 동물의 아이디, 이름, 보호시작일을 이름 순으로 조회하시오. 단, 이름이 같다면 보호를 나중에 시작한 동물을 먼저 보여준다.
+     * SELECT ANIMAL_ID, NAME, DATETIME
+     * FROM ANIMAL_INS
+     * ORDER BY NAME, DATETIME DESC
+     */
+}
