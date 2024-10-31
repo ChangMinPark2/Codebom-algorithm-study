21dee2ae1b65e5156718a091e27db1c1c0fb9eea
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-31 18:06:35 +0900

    feat(프로그래머스): LEVEL1_이름이있는동물의아이디

---
+++ src/프로그래머스_MYSQL/LV1_이름이있는동물의아이디.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_이름이있는동물의아이디 {
+    /**
+     * 이름이 있는 동물의 ID를 조회하는 SQL문을 작성하기, ID 오름차순
+     * SELECT ANIMAL_ID
+     * FROM ANIMAL_INS
+     * WHERE NAME IS NOT NULL
+     * ORDER BY ANIMAL_ID
+     */
+}
