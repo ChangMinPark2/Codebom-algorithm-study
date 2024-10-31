ea4961664f784cb26b827cb51beab1f490fb0b2b
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-31 18:02:53 +0900

    feat(프로그래머스): LEVEL1_강원도에위치한생산공장목록출력하기

---
+++ src/프로그래머스_MYSQL/LV1_강원도에위치한생산공장목록출력하기.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_강원도에위치한생산공장목록출력하기 {
+    /**
+     * 강원도에 위치한 id, 이름, 주소를 id 오름차순으로 조회하는 쿼리 작성하기
+     * SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+     * FROM FOOD_FACTORY
+     * WHERE ADDRESS LIKE '%강원도%'
+     * ORDER BY FACTORY_ID
+     */
+}
