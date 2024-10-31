3a57815137137966ffeb3d5341ce6d33af412cf3
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-31 18:10:33 +0900

    feat(프로그래머스): LEVEL2_중복 제거하기

---
+++ src/프로그래머스_MYSQL/LV2_중복제거하기.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV2_중복제거하기 {
+    /**
+     * 동물의 이름이 몇개인지 조회하는 sql문, null은 집계하지 않고, 종복되는 이름은 하나이다.
+     *
+     * SELECT COUNT(DISTINCT NAME)
+     * FROM ANIMAL_INS
+     * WHERE NAME IS NOT NULL
+     */
+}
