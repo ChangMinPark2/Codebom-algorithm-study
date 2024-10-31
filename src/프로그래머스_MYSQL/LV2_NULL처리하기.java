fd52ea412d617c0479e685e0deee033eafa0c952
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-31 18:19:52 +0900

    feat(프로그래머스): LEVEL2_NULL 처리하기

---
+++ src/프로그래머스_MYSQL/LV2_NULL처리하기.java
@@ -0,0 +1,11 @@
+package 프로그래머스_MYSQL;
+
+public class LV2_NULL처리하기 {
+    /**
+     * 타입, 이름, 성멸 및 중성화 여부를 아이디 순으로 조회하는 sql문을 작성하기
+     * 단, 이름이 없다면 No name으로 표사해 주세요
+     * SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name'), SEX_UPON_INTAKE
+     * FROM ANIMAL_INS
+     * ORDER BY ANIMAL_ID
+     */
+}
