ac15056a4da382819d4956f2baedfadea303a22c
CHANG MIN PARK <98qkrckdals@naver.com>
2024-10-31 18:59:16 +0900

    feat(프로그래머스): LEVEL1_조건에맞는회원수구하기

---
+++ src/프로그래머스_MYSQL/LV1_조건에맞는회원수구하기.java
@@ -0,0 +1,12 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_조건에맞는회원수구하기 {
+    /**
+     * 2021년에 가입한 회원 중 나이가 20~29세 이하인 회원이 몇명인지 출력하는 sql문
+     * -- 코드를 입력하세요
+     * SELECT COUNT(USER_ID)
+     * FROM USER_INFO
+     * WHERE AGE BETWEEN 20 AND 29
+     * AND JOINED LIKE '2021%'
+     */
+}
