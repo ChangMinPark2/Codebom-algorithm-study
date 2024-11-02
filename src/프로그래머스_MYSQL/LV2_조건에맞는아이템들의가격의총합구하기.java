a28466113ed22b2aaf5992390d9ed3d5582b3cea
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-02 19:34:19 +0900

    feat(프로그래머스): LV2_조건에 맞는 아이템들의 가격의 총합 구하기

---
+++ src/프로그래머스_MYSQL/LV2_조건에맞는아이템들의가격의총합구하기.java
@@ -0,0 +1,12 @@
+package 프로그래머스_MYSQL;
+
+public class LV2_조건에맞는아이템들의가격의총합구하기 {
+    /**
+     * ITEM_INFO 테이블에서 희귀도가 'LEGEND'인 아이템들의 가격의 총합을 구하는 SQL문을 작성해 주세요.
+     * 이때 컬럼명은 'TOTAL_PRICE'로 지정해 주세요.
+     *
+     * SELECT SUM(PRICE) as 'TOTAL_PRICE'
+     * FROM ITEM_INFO
+     * WHERE RARITY = 'LEGEND'
+     */
+}
