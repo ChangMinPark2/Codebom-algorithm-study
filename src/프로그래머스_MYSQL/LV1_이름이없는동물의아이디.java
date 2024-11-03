4d79cb6acad764c102fb56344fee46e38f94d2ce
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-03 17:04:24 +0900

    feat(프로그래머스): LV1_이름이없는동물의아이디

---
+++ src/프로그래머스_MYSQL/LV1_이름이없는동물의아이디.java
@@ -0,0 +1,14 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_이름이없는동물의아이디 {
+    /**
+     * 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요.
+     * 단, ID는 오름차순 정렬되어야 합니다.
+     *
+     *
+     * SELECT ANIMAL_ID
+     * FROM ANIMAL_INS
+     * WHERE NAME IS NULL
+     * ORDER BY ANIMAL_ID
+     */
+}
