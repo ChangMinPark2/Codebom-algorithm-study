7788f70073335535d32285a6f2174cc34b41c1a3
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-06 03:01:29 +0900

    feat(프로그래머스): LV1_인기있는아이스크림

---
+++ src/프로그래머스_MYSQL/LV1_인기있는아이스크림.java
@@ -0,0 +1,13 @@
+package 프로그래머스_MYSQL;
+
+public class LV1_인기있는아이스크림 {
+    /**
+     * 상반기에 판매된 아이스크림의 맛을 총주문량을 기준으로 내림차순 정렬하고
+     * 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회하는 SQL 문을 작성해주세요.
+     *
+     *-- 코드를 입력하세요
+     * SELECT FLAVOR
+     * FROM FIRST_HALF
+     * ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID
+     */
+}
