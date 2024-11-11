57498c4737347e8148fb8c4b432f2b703f97ec1e
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-11 19:33:51 +0900

    feat(백준): B2_1550

---
+++ src/백준/왕초보/B2_1550.java
@@ -0,0 +1,14 @@
+package 백준.왕초보;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2_1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(Integer.parseInt(s, 16));
+    }
+}
