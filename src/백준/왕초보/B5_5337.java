3001ffc756f115e424c998079ee8f14ac08e40fb
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-15 20:17:00 +0900

    feat(백준): B5_5337

---
+++ src/백준/왕초보/B5_5337.java
@@ -0,0 +1,9 @@
+package 백준.왕초보;
+
+public class B5_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
