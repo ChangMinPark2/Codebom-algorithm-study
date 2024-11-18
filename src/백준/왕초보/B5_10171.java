0d009f338b4931bc04ea61b1acca17643a96329f
CHANG MIN PARK <98qkrckdals@naver.com>
2024-11-19 04:44:37 +0900

    feat(백준): B5_10171

---
+++ src/백준/왕초보/B5_10171.java
@@ -0,0 +1,10 @@
+package 백준.왕초보;
+
+public class B5_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
