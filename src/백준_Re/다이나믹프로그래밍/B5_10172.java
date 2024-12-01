8f14b591c93309ca3ac609392dd6a2efc5790863
CHANG MIN PARK <98qkrckdals@naver.com>
2024-12-01 23:10:15 +0900

    feat(백준): B5_10172

---
+++ src/백준_Re/다이나믹프로그래밍/B5_10172.java
@@ -0,0 +1,11 @@
+package 백준_Re.다이나믹프로그래밍;
+
+public class B5_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
