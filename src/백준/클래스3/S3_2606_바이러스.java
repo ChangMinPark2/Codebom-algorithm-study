3463dc37bb2694e612937f3a20d3f7af126ae56e
CHANG MIN PARK <98qkrckdals@naver.com>
2025-01-06 23:39:43 +0900

    feat(백준): S3_2606

--- src/백준/클래스3/S3_2606_바이러스.java
+++ src/백준/클래스3/S3_2606_바이러스.java
@@ -3,6 +3,9 @@ package 백준.클래스3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
 
 public class S3_2606_바이러스 {
     static int n, m;
@@ -25,8 +28,26 @@ public class S3_2606_바이러스 {
             arr[b][a] = 1;
         }
 
-        dfs(1);
+        bfs();
         System.out.println(answer);
+    }
+
+    private static void bfs() {
+        visited[1] = true;
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(1);
+
+        while (!q.isEmpty()) {
+            int node = q.remove();
+
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i] && arr[node][i] == 1) {
+                    visited[i] = true;
+                    q.add(i);
+                    answer++;
+                }
+            }
+        }
     }
 
     private static void dfs(int start) {
