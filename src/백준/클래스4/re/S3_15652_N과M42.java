5ccc06c124ba52569492dd773deb41c4be10d7a7
CHANG MIN PARK <98qkrckdals@naver.com>
2025-01-17 18:38:30 +0900

    feat(백준): S2_15652

---
+++ src/백준/클래스4/re/S3_15652_N과M42.java
@@ -0,0 +1,41 @@
+package 백준.클래스4.re;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <= 8 백
+ * 자기자신을 포함하돼, 사전순으로 증가해야함. start, depth
+ */
+public class S3_15652_N과M42 {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] repo = br.readLine().split(" ");
+        n = Integer.parseInt(repo[0]);
+        m = Integer.parseInt(repo[1]);
+        arr = new int[m];
+
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
