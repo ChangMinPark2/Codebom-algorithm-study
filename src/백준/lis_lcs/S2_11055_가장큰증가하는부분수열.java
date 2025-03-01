1919a79618e74f39d27df5b3b0987f1d64911377
CHANG MIN PARK <98qkrckdals@naver.com>
2025-03-01 20:05:26 +0900

    feat(백준): S2_11055_RE

---
+++ src/백준/lis_lcs/S2_11055_가장큰증가하는부분수열.java
@@ -0,0 +1,32 @@
+package 백준.lis_lcs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S2_11055_가장큰증가하는부분수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] mem = new int[n];
+        int answer = -1;
+
+        for (int i = 0; i < n; i++) {
+            mem[i] = arr[i];
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    mem[i] = Math.max(mem[i], mem[j] + arr[i]);
+                }
+            }
+
+            answer = Math.max(answer, mem[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
