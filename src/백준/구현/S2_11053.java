6d51ca77e9e53b0fbaa51a7ec7b6260f91cba180
CHANG MIN PARK <98qkrckdals@naver.com>
2025-03-01 19:37:14 +0900

    feat(백준): S2_11053_RE

---
+++ src/백준/구현/S2_11053.java
@@ -0,0 +1,32 @@
+package 백준.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S2_11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] mem = new int[n];
+
+        mem[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            mem[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && mem[i] < mem[j] + 1) {
+                    mem[i] = mem[j] + 1;
+                }
+            }
+        }
+
+        Arrays.sort(mem);
+
+        System.out.println(mem[n-1]);
+    }
+}
