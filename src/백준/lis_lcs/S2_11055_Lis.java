a0345e4a47d8f13b0827bd1a7686764a2eeed073
CHANG MIN PARK <98qkrckdals@naver.com>
2025-03-12 20:01:01 +0900

    feat(백준): LIS_RE

---
+++ src/백준/lis_lcs/S2_11055_Lis.java
@@ -0,0 +1,31 @@
+package 백준.lis_lcs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class S2_11055_Lis {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] mem = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            mem[i] = arr[i];
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    mem[i] = Math.max(mem[i], mem[j] + arr[i]);
+                }
+            }
+        }
+
+        Arrays.sort(mem);
+
+        System.out.println(mem[n - 1]);
+    }
+}
