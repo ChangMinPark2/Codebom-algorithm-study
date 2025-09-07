8b34b4bb4dac8599134fc99eb3eec4a3a81f1835
CHANG MIN PARK <98qkrckdals@naver.com>
2025-09-08 00:55:39 +0900

    feat(programmers): lv2

---
+++ src/programmers/lv2_전화번호목록.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.*;
+
+public class lv2_전화번호목록 {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+
+        for (String s : phone_book) {
+            set.add(s);
+        }
+
+        for (String s : phone_book) {
+            for (int i = 1; i < s.length(); i++) {
+                if (set.contains(s.substring(0, i))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
