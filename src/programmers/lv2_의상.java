240e351b4a82228952c742bff770e249c2a74fa8
CHANG MIN PARK <98qkrckdals@naver.com>
2025-09-09 18:44:06 +0900

    feat(programmers): lv2

---
+++ src/programmers/lv2_의상.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.*;
+public class lv2_의상 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String[] s : clothes){
+            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
+        }
+
+        for(String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+
+        return answer - 1;
+    }
+}
